0748811fedad50ace8e24222c96e60d8831913ba
pavanjvm <pavanprasad0506@gmail.com>
2026-09-02 09:09:25 +0530

    prime number

--- basic_maths/main.py
+++ basic_maths/main.py
@@ -48,3 +48,14 @@ class Maths:
                     factors.append(n//i)
         factors.sort()
         return factors
+
+
+    def prime_num(self,n:int) -> bool:
+        count =0
+    
+        for i in range(1,int(math.sqrt(n))+1):
+            if n%i == 0:
+                count+=1
+                if n//i != i:
+                    count+=1
+        return count == 2
